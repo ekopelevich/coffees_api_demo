a003945824119407f7813bd53027b834294d6d9b
Elana Kopelevich <ekopelevich@gmail.com>
2016-09-08 14:42:23 -0600

    post request

--- app.js
+++ app.js
@@ -30,10 +30,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res) {
     res.status(err.status || 500)
-    res.render('error', {
-      message: err.message,
-      error: err,
-    })
+    res.send('error')
   })
 }
 
@@ -41,10 +38,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res) {
   res.status(err.status || 500)
-  res.render('error', {
-    message: err.message,
-    error: {},
-  })
+  res.send('error')
 })
 
 
--- routes/coffees.js
+++ routes/coffees.js
@@ -3,12 +3,12 @@ var router = express.Router()
 
 var coffees = [
   {
-    id: 1,
+    id: 4678,
     name: 'Ozo',
     kind: 'good',
   },
   {
-    id: 2,
+    id: 84876,
     name: 'Boxcar',
     kind: 'better',
   },
@@ -16,6 +16,37 @@ var coffees = [
 
 router.get('/', function(req, res) {
   res.send(coffees)
+})
+
+
+// router.get('/:id', function(req, res){
+//   var id = req.params.id
+//   for (var i = 0; i < coffees.length; i++) {
+//     if (id == coffees[i].id) {
+//       res.send(coffees[i])
+//     }
+//   }
+// })
+
+router.get('/:id', function(req, res) {
+  var id = parseInt(req.params.id)
+  coffees.map(function(coffee){
+    if (id === coffee.id) {
+      res.send(coffee)
+    }
+  })
+  res.send('There is no coffee with that index!')
+})
+
+router.post('/', function(req, res){
+  // console.log(req.body)
+  var id = parseInt(req.body.id)
+  coffees.push({
+    id: id,
+    name: req.body.name,
+    kind: req.body.kind,
+  })
+  res.redirect('/api/v1/coffees')
 })
 
 module.exports = router
